8b21752b420548b430c40332d791094aa7f87cd4
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-03-14 10:37:13 -0500

    feat(item): implemented item in redis
    additionally reset repo due to page caching issue

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -4,15 +4,15 @@ import { client } from '../src/services/redis';
 const run = async () => {
 	await client.hSet('car', {
 		color: 'red',
-		year: 1950,
+		year: 1950
 	});
 
 	const car = await client.hGetAll('car');
-  
-  if (Object.keys(car).length === 0) {
-    console.log('Car not found, respond with 404')
-    return
-  }
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car not found, respond with 404');
+		return;
+	}
 
 	console.log(car);
 };
